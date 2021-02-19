c7b0dad00dd7cb568a1c04b4109587d2925f732b
YaFred <yafred@users.noreply.github.com>
2021-02-19 06:42:59 +0100

    fix embed

--- internal/embed/embed.go
+++ internal/embed/embed.go
@@ -4,6 +4,9 @@ import (
 	"embed"
 )
 
-//go:embed web
+//go:embed css
+//go:embed img
+//go:embed javascript
+//go:embed index.html
 // StaticFiles ... files that will be embedded in the application
 var StaticFiles embed.FS
